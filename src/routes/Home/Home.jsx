ef8b2ff8b74cb0dd465cce7ec3792fde558175bb
blake <blake.netzeband@spiredigital.com>
2020-03-19 18:40:04 -0600

    Added content to home page

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,12 +1,30 @@
-import { useState, useEffect } from "preact/hooks";
-import PropTypes from "prop-types";
-
-function Home(props) {
+export default function Home() {
   return (
     <section class="home">
-      <h1>HOME</h1>
+      <section class="title">
+        <div class="bg-img" />
+
+        <article class="title-content">
+          <h1>Kalli Bear Films</h1>
+        </article>
+      </section>
+
+      <section class="content">
+        <h2>A Day To Remember</h2>
+        <article class="testimonies">
+          <em>
+            "It’s impossible to see all the <b>micro moments</b> the day of your
+            wedding. Through our video, we were able to see our parents dancing
+            cheek to cheek and our father-in-law cry as we read our vows. And{" "}
+            <b>that was just the beginning!"</b>
+          </em>
+          <em>"Video captures movement and sound."</em>
+          <em>
+            "You’ll laugh, you’ll cry - It’s the closest you’ll get to reliving{" "}
+            <b>your wedding day.</b>""
+          </em>
+        </article>
+      </section>
     </section>
   );
 }
-
-export default Home;
