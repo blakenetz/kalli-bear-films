a7ae11a35916d0a192ffc1a110691d6f89e087b6
blake <blake.netzeband@spiredigital.com>
2020-03-22 13:42:10 -0600

    Updates

---
+++ precache-manifest.2e9d6a93b1e16658bd9fb41ca0c165aa.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "df65f1e288ba1d3b932a32347daba083",
+    "url": "/assets/images/beach-couple.jpg"
+  },
+  {
+    "revision": "856adb8da659d41f66f3edfa941e4830",
+    "url": "/assets/images/black-and-white-couple-formal.jpg"
+  },
+  {
+    "revision": "d974e1bd2157fba196a72a24f93442bd",
+    "url": "/assets/images/black-and-white-couple-kiss.jpg"
+  },
+  {
+    "revision": "781a6266cec19232a842644166443422",
+    "url": "/assets/images/confitti-couple.jpg"
+  },
+  {
+    "revision": "4cf7e10e82f634acbf89a2a246149e84",
+    "url": "/assets/images/landscape-couple.jpg"
+  },
+  {
+    "revision": "f1ca8d7f7b18e07cfc6880d1e2418c1b",
+    "url": "/assets/images/logo.png"
+  },
+  {
+    "revision": "c02204e4edabbde630b0236506e141a1",
+    "url": "/assets/images/sunflower-couple.jpg"
+  },
+  {
+    "revision": "9452da672beb0b74666ef8dfa9356c79",
+    "url": "/assets/images/thats-me.png"
+  },
+  {
+    "revision": "d1c43c57e39cf552cf7a",
+    "url": "/bundle.04035.css"
+  },
+  {
+    "revision": "d1c43c57e39cf552cf7a",
+    "url": "/bundle.d1c43.js"
+  },
+  {
+    "revision": "2bf59a2139588f4f6358c16066930b78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09446198657efe85c470",
+    "url": "/polyfills.09446.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "c2beec73252e143efbe23ec366d2e3ae",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
