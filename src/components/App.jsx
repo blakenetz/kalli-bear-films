8e2cc6938191d9f1a4040d461210f7e0e04334c9
blake <blake.netzeband@spiredigital.com>
2020-03-22 13:50:11 -0600

    added nav state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from "preact";
 import { Router } from "preact-router";
+import classnames from "classnames";
 
 // Code-splitting is automated for routes
 import Home from "../routes/Home/Home";
@@ -9,17 +10,22 @@ import Packages from "../routes/Packages/Packages";
 import Contact from "../routes/Contact/Contact";
 
 export default class App extends Component {
+  state = {
+    hasNav: false
+  };
+
   /** Gets fired when the route changes.
    *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
    *	@param {string} event.url	The newly routed URL
    */
   handleRoute = e => {
     this.currentUrl = e.url;
+    this.setState({ hasNav: e.url !== "/" });
   };
 
   render() {
     return (
-      <main id="app">
+      <main id="app" class={classnames({ "with-nav": this.state.hasNav })}>
         <Router onChange={this.handleRoute}>
           <Home path="/" />
           <Work path="/work" />
