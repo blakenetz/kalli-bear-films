b443096c16670c56c84c7eb2a8aa12ae34982e7f
blake <blake.netzeband@spiredigital.com>
2020-03-28 19:59:24 -0600

    added error color. added grid to portfolio. added ready and click animations. stack poster over youtube video

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -1,7 +1,10 @@
-import YouTube from "react-youtube";
+/* eslint-disable react/jsx-no-bind */
 import { useState } from "preact/hooks";
+import YouTube from "react-youtube";
+import classnames from "classnames";
 
 import withNav from "../../components/HOC/WithNav/WithNav";
+import { ErrorSVG, PlaySVG } from "../../components/svg/VideoControls";
 
 const urls = [
   { id: "GUiVqvsaOww", couple: "Luke and Casey" },
@@ -13,52 +16,78 @@ const urls = [
   { id: "UGOxgwgplDo", couple: "York and Amanda" }
 ];
 
-function Work() {
+function Portfolio() {
+  // track a single id
   const [targetVideo, setTargetVideo] = useState();
+  const [readyVideo, setReadyVideo] = useState();
+  // array of ids
+  const [errorVideos, setErrorVideos] = useState([]);
+  // iframe options
+  const [opts, setOpts] = useState({ width: 0, height: 0 });
+
+  function handleClick(id, e) {
+    // match height of image element
+    const { width, height } = e.currentTarget.getBoundingClientRect();
+    setOpts({ width, height: height - height * 0.265 });
+    // show youtube iframe
+    setTargetVideo(id);
+  }
+
+  function handleReady(id) {
+    setReadyVideo(id);
+  }
+
+  function handleError(id) {
+    setErrorVideos([...errorVideos, id]);
+  }
 
   return (
-    <section id="portfolio">
+    <section class="portfolio">
       <h1>Portfolio</h1>
-      <article class="grid">
-        {urls.map(({ id, couple }) => (
-          <div class="video">
-            <img
-              src={`https://img.youtube.com/vi/${id}/0.jpg`}
-              alt={`Wedding video for ${couple}`}
-            />
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              alt="play video"
-            >
-              <polygon points="5 3 19 12 5 21 5 3" />
-            </svg> */}
-            <YouTube
-              videoId={id}
-              id={id}
-              containerClassName="video-iframe"
-              // opts={obj}
-              // onReady={func}
-              // onPlay={func}
-              // onPause={func}
-              // onEnd={func}
-              // onError={func}
-              // onStateChange={func}
-              // onPlaybackRateChange={func}
-              // onPlaybackQualityChange={func}
-            />
-          </div>
-        ))}
-      </article>
+      <section class="grid">
+        {urls.map(({ id, couple }) => {
+          const isError = errorVideos.indexOf(id) !== -1;
+
+          return (
+            <article class="cell">
+              <figure
+                class={classnames("video", {
+                  faded: targetVideo === id,
+                  error: isError
+                })}
+                onClick={isError ? null : handleClick.bind(null, id)}
+              >
+                <img
+                  src={`https://img.youtube.com/vi/${id}/hqdefault.jpg`}
+                  alt={`Wedding video for ${couple}`}
+                />
+                {isError ? <ErrorSVG /> : <PlaySVG />}
+              </figure>
+
+              {id === targetVideo && (
+                <YouTube
+                  videoId={id}
+                  id={id}
+                  containerClassName={classnames("video-iframe", {
+                    ready: readyVideo === id
+                  })}
+                  opts={opts}
+                  onReady={handleReady.bind(null, id)}
+                  onError={handleError.bind(null, id)}
+                  // onPlay={func}
+                  // onPause={func}
+                  // onEnd={func}
+                  // onStateChange={func}
+                  // onPlaybackRateChange={func}
+                  // onPlaybackQualityChange={func}
+                />
+              )}
+            </article>
+          );
+        })}
+      </section>
     </section>
   );
 }
 
-export default withNav(Work);
+export default withNav(Portfolio);
